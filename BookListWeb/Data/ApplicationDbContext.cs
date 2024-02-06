bd71c20c4bc2bad7aa1e714c691011a2fd3dba3f
Malina Teodora <albumalina26@gmail.com>
2024-02-06 21:32:32 +0200

    Add Indetity

--- BookListWeb/Data/ApplicationDbContext.cs
+++ BookListWeb/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using BookListWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookListWeb.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -15,6 +16,8 @@ namespace BookListWeb.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                new Category { Id = 2, Name = "Drama", DisplayOrder = 2 },
