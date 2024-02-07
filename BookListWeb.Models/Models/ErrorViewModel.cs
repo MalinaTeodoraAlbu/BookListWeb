0e483fb3bb67b30b0678d7824fa43314a00b04f2
Malina Teodora <albumalina26@gmail.com>
2024-02-07 13:25:09 +0200

    Identity Regist

--- BookListWeb.Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookListWeb.Models.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
